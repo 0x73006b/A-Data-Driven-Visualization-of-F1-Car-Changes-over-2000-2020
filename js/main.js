13ba5f9dbab3ee43e508bdf9bc1a5f1f26940b31
Shabab <khan.k.shabab@gmail.com>
2021-03-23 00:49:33 -0700

    Basic Mechanical Changes Scatterplot created - added mC, tooltip div - added tooltip css - added basic call to main.js - added basic code to mechanicalChanges.js; pulled from my p2

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
-// Todo: set up extra data processing, if any here
-// Todo: create instance of the charts
+let mechanicalChanges;
+let mechanicalChangesData;
+
+// TODO: Remove the eslint disables once ready
+// eslint-disable-next-line no-unused-vars
+let lapTime0;
+// eslint-disable-next-line no-unused-vars
+let lapTime1;
+let circuitData;
+
+// TODO: Setup charts here
+Promise.all([
+  d3.json('data/Filtered Merged Circuit.json'),
+  d3.json('data/car_data.json'),
+])
+  .then((data) => {
+    // TODO: Remove eslint disable once utilized
+    // eslint-disable-next-line no-unused-vars
+    [circuitData, mechanicalChangesData] = data;
+
+    // Create Mechanical Changes Scatterplot
+    mechanicalChanges = new ScatterPlot({ parentElement: '#mechanical-changes' }, mechanicalChangesData.data);
+  });
