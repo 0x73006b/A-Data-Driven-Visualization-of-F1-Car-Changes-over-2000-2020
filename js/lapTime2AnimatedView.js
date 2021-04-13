eff4f6652c01b085c3bb25a625f87cc5954026f6
RobertMo <robertmozhaoming@gmail.com>
2021-04-12 19:57:07 -0700

    rename track svg files

--- js/lapTime2AnimatedView.js
+++ js/lapTime2AnimatedView.js
@@ -44,28 +44,23 @@ class RealTimeLap {
 
     vis.iterateNum = 0;
     vis.selectedTrack = '';
-    vis.startLap = -1;
     vis.trackColor = [['#f20002', '#2fb2e3', '#fcd303'], ['#13C296', '#F5D900', '#F63EBA']];
-    vis.sectorTime = [[19826, 35241, 20922], [18638, 33392, 18779]];
     vis.updateVis();
   }
 
   /**
    * Prepare data and scales before we render it
    */
-  updateVis(_selectedTrack, _startLap) {
+  updateVis(_selectedTrack) {
     const vis = this;
     vis.iterateNum += 1;
     vis.selectedTrack = _selectedTrack;
-    vis.startLap = _startLap;
     vis.renderVis();
   }
 
   renderVis() {
     const vis = this;
-    const laps = vis.data.filter((d) => d.circuitName === vis.selectedTrack);
-    const laptimes = laps.map((d) => d.laptimeMillis).sort();
-
+    vis.laps = vis.data.filter((d) => d.circuitName === vis.selectedTrack);
     if (this.selectedTrack === '') {
       d3.select('#laptime2-reatimeLap').selectAll('*').remove();
       const ANIMATION_SVG = d3.select('#laptime2-reatimeLap')
@@ -78,47 +73,44 @@ class RealTimeLap {
         .attr('y', 50)
         .attr('dy', '.35em')
         .text('Please select a track to race');
-    } else if (laps.length > 0) {
-      if (this.startLap === -1) {
-        const fileName = `data/maps/${laps[0].circuitRef}_sector_double.svg`;
-        d3.xml(fileName)
-          .then((data) => {
-          // clean up previous drawings
-            d3.select('#laptime2-reatimeLap').selectAll('*').remove();
-            d3.select('#laptime2-reatimeLap').node().append(data.documentElement);
-            console.log(d3.select('#tracksvg'));
-            // setup background
-            const background = d3.select('#background');
-            background
-              .attr('stroke-width', 15)
-              .transition()
-              .ease(d3.easeLinear)
-              .attr('stroke-dashoffset', 0);
+    } else if (vis.laps.length > 0) {
+      const fileName = `data/maps/${vis.laps[0].circuitName}.svg`;
+      d3.xml(fileName)
+        .then((data) => {
+        // clean up previous drawings
+          d3.select('#laptime2-reatimeLap').selectAll('*').remove();
+          d3.select('#laptime2-reatimeLap').node().append(data.documentElement);
+          // setup background
+          const background = d3.select('#background');
+          background
+            .attr('stroke-width', 15)
+            .transition()
+            .ease(d3.easeLinear)
+            .attr('stroke-dashoffset', 0);
 
-            vis.animationPaths = [[], []];
-            // draw out actual lap
-            for(var i = 0; i <3; i++) {
-              vis.animationPaths[0][i] = d3.select(`#sector0${i}`);
-              vis.animationPaths[1][i] = d3.select(`#sector1${i}`);
-              vis.animationPaths[0][i]
-                .attr('stroke-width', 0);
-              vis.animationPaths[1][i]
-                .attr('stroke-width', 0);
-            }
-          }).catch(() => {
-            d3.select('#laptime2-reatimeLap').selectAll('*').remove();
-            const ANIMATION_SVG = d3.select('#laptime2-reatimeLap')
-              .append('svg')
-              .attr('width', 1000)
-              .attr('height', 800);
+          vis.animationPaths = [[], []];
+          // draw out actual lap
+          for(var i = 0; i <3; i++) {
+            vis.animationPaths[0][i] = d3.select(`#sector0${i}`);
+            vis.animationPaths[1][i] = d3.select(`#sector1${i}`);
+            vis.animationPaths[0][i]
+              .attr('stroke-width', 0);
+            vis.animationPaths[1][i]
+              .attr('stroke-width', 0);
+          }
+        }).catch(() => {
+          d3.select('#laptime2-reatimeLap').selectAll('*').remove();
+          const ANIMATION_SVG = d3.select('#laptime2-reatimeLap')
+            .append('svg')
+            .attr('width', 1000)
+            .attr('height', 800);
 
-            ANIMATION_SVG.append('text')
-              .attr('x', 50)
-              .attr('y', 50)
-              .attr('dy', '.35em')
-              .text('Selected track is not available for animation');
-          });
-      }
+          ANIMATION_SVG.append('text')
+            .attr('x', 50)
+            .attr('y', 50)
+            .attr('dy', '.35em')
+            .text('Selected track is not available for animation');
+        });
     }
     // start button
     d3.select('#startButton')
@@ -145,7 +137,7 @@ class RealTimeLap {
         .attr('stroke-dasharray', `${sectorLength} ${sectorLength}`)
         .attr('stroke-dashoffset', sectorLength)
         .transition()
-        .duration(vis.sectorTime[trackNum][sectorNum])
+        .duration(vis.laps[trackNum]['sector' + (sectorNum + 1) ] * 1000)
         .ease(d3.easeLinear)
         .attr('stroke-dashoffset', 0)
         .on('end', () => {
